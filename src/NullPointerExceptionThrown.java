5c34ef29ff5208f09733c6eaef22c85b62f8ac16
Allison Snipes <ajsnipes@usfca.edu>
2020-07-22 14:51:24 -0700

    first commit

---
+++ src/NullPointerExceptionThrown.java
@@ -0,0 +1,30 @@
+/*
+ * Allison Snipes
+ * Course 605.201.81 Summer 2020
+ * Assignment 9 Question 1
+ * 
+ *  Project Specs:
+ *  This programming assignment involves learning about some of the common exceptions that occur in Java programs.
+ *  Consider the following exception types: NullPointerException, ArrayIndexOutOfBoundsException, ClassCastException,
+ *  and IllegalArgumentException. Research what each exception type means and the conditions under which each occurs 
+ *  (i.e., is thrown). 
+ *  
+ *  Deliverables:
+ *  Write programs that demonstrate each type of exception being thrown (one program per exception) and provide a 
+ *  screen capture of the output. Name your programs as follows: NullPointerExceptionThrown, etc.
+ *  	1. write a program that catches each type of thrown exception and display an error message indicating some of
+ *  	   the details as to what that exception is. 
+ *  	2. Write one program per type of exception. Name your programs as follows: NullPointerExceptionCatch, etc.
+
+/*
+ * Here I will set up the main class here which will have the required elements that will create the 2
+ * objects of each type (animal and vehicle). This is where the whole project comes together with the
+ * interfaces and abstract classes.
+ */
+public class NullPointerExceptionThrown {
+
+	public static void main(String[] args) {
+		System.out.println("works");
+	}
+
+}
