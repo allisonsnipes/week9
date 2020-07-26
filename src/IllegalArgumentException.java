0208881d3f0db29a46eeeebb34fd48a56f0eb3ee
Allison Snipes <ajsnipes@usfca.edu>
2020-07-26 06:40:00 -0700

    finished 4/4

--- src/IllegalArgumentException.java
+++ src/IllegalArgumentException.java
@@ -15,24 +15,41 @@
  *  	1. write a program that catches each type of thrown exception and display an error message indicating some of
  *  	   the details as to what that exception is. 
  *  	2. Write one program per type of exception. Name your programs as follows: NullPointerExceptionCatch, etc.
+ */
+
+import java.util.Scanner;
 
 /*
  * Here I will set up my classes and subclasses which will be needed to throw the NullPointerExceptionThrown for the Employee class.
  */
-public class IllegalArgumentException {
+public class IllegalArgumentException extends Throwable {
 
 	public static void main(String[] args) {
 		headerMsg();
+		try {
+			System.out.println("Please enter a random number to obtain a percentage: \n");
+			Scanner input = new Scanner(System.in);
+			int percent = input.nextInt();
+			if (percent > 100 || percent < 0) {
+				throw new IllegalArgumentException();
+			}
+		} catch(IllegalArgumentException e) {
+			System.out.println("There is an error: ");
+			e.printStackTrace();
+		}
+		
+		
+		
 
 	}
 	
 	public static void headerMsg() {
 		System.out.println("\n");
 		System.out.println("+------------------------------------------------------------------------------------+");
-		System.out.println("|                                    Welcome User,                                   |");
-		System.out.println("|      This application will demonstrate how a NullPointerException is generated.    |");
-		System.out.println("|   This type of exception is thrown when an application attempts to use null in a   |");
-		System.out.println("|          case where an object is required. Let's view this example!                |");
+		System.out.println("|                                         Welcome User,                              |");
+		System.out.println("|    This application will demonstrate how a IllegalArgumentException is generated.  |");
+		System.out.println("|       This type of exception is thrown when a method is passed by an illegal or    |");
+		System.out.println("|                    inappropriate argument. Let's view this example!                |");
 		System.out.println("+------------------------------------------------------------------------------------+");
 		System.out.println("\n");
 	}
